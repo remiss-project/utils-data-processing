fa0bb2ecb070889c3cd79b3ab1073780d8e99da5
Joan Massachs <joan.massachs@eurecat.org>
2023-03-15 12:55:36 +0100

    identació correcta

--- get_most_common_words.py
+++ get_most_common_words.py
@@ -11,22 +11,22 @@ consulta = sys.argv[1]
 
 
 def get_words(text):
-  text = text.lower()
-  text = re.sub('(\w+:\/\/\S+)', '', text) #URL fora
-  text = re.sub('[^ a-z0-9_#@ñçáéíóúàèìòùäëïöü]', ' ', text)
-  return text.split()
+    text = text.lower()
+    text = re.sub('(\w+:\/\/\S+)', '', text) #URL fora
+    text = re.sub('[^ a-z0-9_#@ñçáéíóúàèìòùäëïöü]', ' ', text)
+    return text.split()
 
 
 counts = Counter()
 with open(consulta) as f:
-  lines = sum(1 for line in f)
+    lines = sum(1 for line in f)
 with open(consulta) as f:
-  for line in tqdm(f, total=lines):
-    for t in ensure_flattened(json.loads(line)):
-      text = t['text']
-      words = get_words(text)
-      counts.update(words)
+    for line in tqdm(f, total=lines):
+        for t in ensure_flattened(json.loads(line)):
+            text = t['text']
+            words = get_words(text)
+            counts.update(words)
 
 with open('words.csv', 'w') as f:
-  for w, c in counts.most_common():
-    f.write(w + ',' + str(c) + '\n')
+    for w, c in counts.most_common():
+        f.write(w + ',' + str(c) + '\n')
