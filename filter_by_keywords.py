0c4ff59db8a3cbecb06499066878dfac4f6f4e8e
Joan Massachs <joan.massachs@eurecat.org>
2023-03-16 09:41:38 +0100

    filter by keywords: file arguments

--- filter_by_keywords.py
+++ filter_by_keywords.py
@@ -27,26 +27,22 @@ def get_text(tweet):
 
 @click.command()
 @click.option('--count-lines', '-c', is_flag=True, default=False)
-@click.argument('keywords')
-@click.argument('infile')
-@click.argument('outfile')
+@click.argument('keywords', type=click.File('r'))
+@click.argument('infile', type=click.File('r'))
+@click.argument('outfile', type=click.File('w'))
 def main(count_lines, keywords, infile, outfile):
-    with open(keywords) as f:
-        keywords = [strip_accents(k) for k in json.load(f)['keywords']]
+    keywords = [strip_accents(k) for k in json.load(keywords)['keywords']]
 
     if count_lines:
-        with open(infile) as f:
-            lines = sum(1 for line in f)
+        lines = sum(1 for line in infile)
     else:
         lines = None
 
-    with open(infile) as infile:
-        with open(outfile, 'w') as outfile:
-            for line in tqdm(infile, total=lines):
-                for t in ensure_flattened(json.loads(line)):
-                    text = strip_accents(get_text(t))
-                    if contains_keywords(text, keywords):
-                        outfile.write(json.dumps(t) + '\n')
+    for line in tqdm(infile, total=lines):
+        for t in ensure_flattened(json.loads(line)):
+            text = strip_accents(get_text(t))
+            if contains_keywords(text, keywords):
+                outfile.write(json.dumps(t) + '\n')
 
 
 if __name__ == '__main__':
