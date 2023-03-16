25aef94b4ce81398832667e947b1f95ec5a00263
Joan Massachs <joan.massachs@eurecat.org>
2023-03-16 09:54:31 +0100

    merge: output file

--- merge.py
+++ merge.py
@@ -8,19 +8,18 @@ from twarc import ensure_flattened
 
 @click.command()
 @click.argument('inpath')
-@click.argument('outpath')
-def main(inpath, outpath):
+@click.argument('outfile', type=click.File('w'))
+def main(inpath, outfile):
     tids = set()
     files = [f for f in os.listdir(inpath) if '.jsonl' in f]
     for f in tqdm(files):
-        with open(inpath + f) as inf:
-            for line in inf:
+        with open(inpath + f) as infile:
+            for line in infile:
                 for tweet in ensure_flattened(json.loads(line)):
                     tid = tweet['id']
                     if tid not in tids:
-                        with open(outpath, 'a') as outf:
-                            tids |= {tid}
-                            outf.write(json.dumps(tweet) + '\n')
+                        tids |= {tid}
+                        outfile.write(json.dumps(tweet) + '\n')
 
 
 if __name__ == '__main__':
