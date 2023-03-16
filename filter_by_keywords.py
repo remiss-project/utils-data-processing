9d409a01439b3d735f4e1fff0274ef350f45b2e9
Joan Massachs <joan.massachs@eurecat.org>
2023-03-16 09:48:34 +0100

    filter by keywords: FileSizeProgressBar

--- filter_by_keywords.py
+++ filter_by_keywords.py
@@ -2,8 +2,8 @@ import json
 import sys
 
 import click
-from tqdm import tqdm
 from twarc import ensure_flattened
+from twarc.decorators2 import FileSizeProgressBar
 
 from strip_accents import strip_accents
 
@@ -26,23 +26,19 @@ def get_text(tweet):
 
 
 @click.command()
-@click.option('--count-lines', '-c', is_flag=True, default=False)
 @click.argument('keywords', type=click.File('r'))
 @click.argument('infile', type=click.File('r'))
 @click.argument('outfile', type=click.File('w'))
-def main(count_lines, keywords, infile, outfile):
+def main(keywords, infile, outfile):
     keywords = [strip_accents(k) for k in json.load(keywords)['keywords']]
 
-    if count_lines:
-        lines = sum(1 for line in infile)
-    else:
-        lines = None
-
-    for line in tqdm(infile, total=lines):
-        for t in ensure_flattened(json.loads(line)):
-            text = strip_accents(get_text(t))
-            if contains_keywords(text, keywords):
-                outfile.write(json.dumps(t) + '\n')
+    with FileSizeProgressBar(infile, outfile) as progress:
+        for line in infile:
+            for t in ensure_flattened(json.loads(line)):
+                text = strip_accents(get_text(t))
+                if contains_keywords(text, keywords):
+                    outfile.write(json.dumps(t) + '\n')
+            progress.update(len(line))
 
 
 if __name__ == '__main__':
