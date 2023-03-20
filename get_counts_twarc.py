f037c63249f0d311a485b5d851afdf2708fd2191
Joan Massachs <joan.massachs@eurecat.org>
2023-03-20 16:02:15 +0100

    get_counts_twarc: granularity

--- get_counts_twarc.py
+++ get_counts_twarc.py
@@ -7,13 +7,18 @@ import click
 @click.command()
 @click.argument('infile', type=click.File('r'))
 @click.argument('outfile')
-def main(infile, outfile):
+@click.option(
+    '--granularity', 'granularity',
+    type=click.Choice(['day', 'hour', 'minute']), default='day'
+)
+def main(infile, outfile, granularity):
     for window in json.load(infile):
         with open('tmp.txt', 'w') as t:
             for keyword in window['keywords']:
                 t.write(keyword + '\n')
         subprocess.run([
             'twarc2', 'searches', '--archive', '--counts-only',
+            '--granularity', granularity,
             '--start-time', window['start-time'],
             '--end-time', window['end-time'],
             'tmp.txt', outfile
