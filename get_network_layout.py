36210954ead5240dd1fb78ef13fc82a5504bfe30
Joan Massachs <joan.massachs@eurecat.org>
2023-03-20 08:46:47 +0100

    més layouts

--- get_network_layout.py
+++ get_network_layout.py
@@ -5,6 +5,12 @@ import click
 import networkx as nx
 
 
+def save_layout(layout, outfile):
+    layout = {k: list(v) for k, v in layout.items()}
+    with open(outfile, 'w') as outf:
+        json.dump(layout, outf)
+
+
 @click.command()
 @click.argument('infile')
 @click.argument('outfile')
@@ -12,8 +18,13 @@ import networkx as nx
     '--component', 'component',
     type=click.Choice(['weak', 'strong']), default=None
 )
+@click.option(
+    '--layout', 'layout',
+    type=click.Choice(['kamada_kawai', 'spectral', 'spring']),
+    default='kamada_kawai'
+)
 @click.option('--weight', 'weight', default='weight')
-def main(infile, outfile, component, weight):
+def main(infile, outfile, component, layout, weight):
     graph = nx.read_gml(infile)
     null_edges = [
         (x, y)
@@ -30,18 +41,27 @@ def main(infile, outfile, component, weight):
         graph = graph.subgraph(gcc)
     graph = graph.to_undirected()
 
-    layout = None
-    if isfile(outfile):
-        with open(outfile) as inf:
-            layout = json.load(inf)
-    while True:
-        layout = nx.spring_layout(
-            graph, weight=weight,
-            pos=layout, iterations=1
-        )
-        layout = {k: list(v) for k, v in layout.items()}
-        with open(outfile, 'w') as outf:
-            json.dump(layout, outf)
+    if layout == 'spring':
+        layout = None
+        if isfile(outfile):
+            with open(outfile) as inf:
+                layout = json.load(inf)
+        while True:
+            layout = nx.spring_layout(
+                graph, weight=weight,
+                pos=layout, iterations=1
+            )
+            save_layout(layout, outfile)
+    else:
+        if layout == 'kamada_kawai':
+            weights = nx.get_edge_attributes(graph, weight)
+            costs = {k: 1/v for k, v in weights.items()}
+            nx.set_edge_attributes(graph, costs, 'cost')
+            dist = nx.shortest_path_length(graph, weight='cost')
+            layout = nx.kamada_kawai_layout(graph, weight=weight, dist=dist)
+        else:
+            layout = nx.spectral_layout(graph, weight=weight)
+        save_layout(layout, outfile)
 
 
 if __name__ == '__main__':
