11bdd8b85adda8570a96eeef14188148a83c0de9
Joan Massachs <joan.massachs@eurecat.org>
2023-04-18 15:20:59 +0200

    get network layout: més d'una xarxa

--- get_network_layout.py
+++ get_network_layout.py
@@ -1,23 +1,12 @@
 import json
+import os
 
 import click
 import graph_tool.all as gt
+from tqdm import tqdm
 
 
-@click.command()
-@click.argument('infile', type=click.Path(exists=True))
-@click.argument('outfile', type=click.File('w'))
-@click.option(
-    '--component', 'component',
-    type=click.Choice(['weak', 'strong']), default=None
-)
-@click.option(
-    '--layout', 'layout',
-    type=click.Choice(['sfdp', 'arf', 'fr']),
-    default='sfdp', show_default=True
-)
-@click.option('--weight', 'weight', default='weight', show_default=True)
-def main(infile, outfile, component, layout, weight):
+def get_network_layout(infile, outfile, component, layout, weight):
     g = gt.load_graph(infile)
 
     non_null_edges = [g.ep[weight][e] > 0 for e in g.edges()]
@@ -35,7 +24,33 @@ def main(infile, outfile, component, layout, weight):
         layout = gt.fruchterman_reingold_layout(g, weight=g.ep[weight])
 
     layout = {g.vp.label[v]: list(layout[v]) for v in g.vertices()}
-    json.dump(layout, outfile)
+    with open(outfile, 'w') as f:
+        json.dump(layout, f)
+
+
+@click.command()
+@click.argument(
+    'infiles', type=click.Path(exists=True, dir_okay=False), nargs=-1
+)
+@click.argument(
+    'outdir', type=click.Path(exists=True, file_okay=False, writable=True)
+)
+@click.option(
+    '--component', 'component',
+    type=click.Choice(['weak', 'strong']), default=None
+)
+@click.option(
+    '--layout', 'layout',
+    type=click.Choice(['sfdp', 'arf', 'fr']),
+    default='sfdp', show_default=True
+)
+@click.option('--weight', 'weight', default='weight', show_default=True)
+def main(infiles, outdir, component, layout, weight):
+    infiles = [f for f in infiles if f.endswith('.gml')]
+    for infile in tqdm(infiles):
+        name = os.path.basename(infile)[:-4]
+        outfile = os.path.join(outdir, name + '.csv')
+        get_network_layout(infile, outfile, component, layout, weight)
 
 
 if __name__ == '__main__':
