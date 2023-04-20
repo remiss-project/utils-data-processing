a270cc673300f35311e7f303d7b81189c9c98eff
Joan Massachs <joan.massachs@eurecat.org>
2023-04-20 09:15:48 +0200

    get network communities: més d'una xarxa

--- get_network_communities.py
+++ get_network_communities.py
@@ -1,17 +1,13 @@
 import json
+import os
 
 import click
 import networkx as nx
 import networkx.algorithms.community as nx_comm
+from tqdm import tqdm
 
 
-@click.command()
-@click.argument('network', type=click.Path(exists=True, dir_okay=False))
-@click.argument('outfile', type=click.File('w'))
-@click.option('--weight', 'weight', default='weight', show_default=True)
-@click.option('--cutoff', type=click.IntRange(1), default=1, show_default=True)
-@click.option('--best_n', type=click.IntRange(1), default=None)
-def main(network, outfile, weight, cutoff, best_n):
+def get_network_communities(network, outfile, weight, cutoff, best_n):
     graph = nx.read_gml(network)
 
     null_edges = [
@@ -30,7 +26,26 @@ def main(network, outfile, weight, cutoff, best_n):
     )
     comm = sorted(comm, key=len, reverse=True)
     comm = {node: i for i, c in enumerate(comm) for node in c}
-    json.dump(comm, outfile)
+    with open(outfile, 'w') as f:
+        json.dump(comm, f)
+
+
+@click.command()
+@click.argument(
+    'infiles', type=click.Path(exists=True, dir_okay=False), nargs=-1
+)
+@click.argument(
+    'outdir', type=click.Path(exists=True, file_okay=False, writable=True)
+)
+@click.option('--weight', 'weight', default='weight', show_default=True)
+@click.option('--cutoff', type=click.IntRange(1), default=1, show_default=True)
+@click.option('--best_n', type=click.IntRange(1), default=None)
+def main(infiles, outdir, weight, cutoff, best_n):
+    infiles = [f for f in infiles if f.endswith('.gml')]
+    for infile in tqdm(infiles):
+        name = os.path.basename(infile)[:-4]
+        outfile = os.path.join(outdir, name + '.json')
+        get_network_communities(infile, outfile, weight, cutoff, best_n)
 
 
 if __name__ == '__main__':
