ec686e9024a4b5ad0fc04620b5fc98f059418b60
Joan Massachs <joan.massachs@eurecat.org>
2023-07-05 13:52:24 +0200

    strip accents: lower

--- strip_accents.py
+++ strip_accents.py
@@ -4,4 +4,4 @@ import unicodedata
 def strip_accents(text):
     text = unicodedata.normalize('NFD', text)
     text = [c for c in text if unicodedata.category(c) != 'Mn']
-    return ''.join(text)
+    return ''.join(text).lower()
